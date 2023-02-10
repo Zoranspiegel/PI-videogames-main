03fdaa3cf61b6cafbdd72ce2db21425df839f097
Juan José Rodriguez <zoranbow@gmail.com>
2023-02-10 18:32:07 -0500

    C:/Program Files/Git/api deploy

--- api/src/db.js
+++ api/src/db.js
@@ -2,15 +2,26 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 
+//DEPLOYMENT
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`,
+  DB_DEPLOY,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   }
 );
+
+//DEVELOPMENT
+// const sequelize = new Sequelize(
+//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`,
+//   {
+//     logging: false, // set to console.log to see the raw SQL queries
+//     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//   }
+// );
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
