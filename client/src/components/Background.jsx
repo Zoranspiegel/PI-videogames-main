ee78bf31254809a20fd601220a82cdda63486e98
Juan José Rodriguez Valcke <zoranbow@gmail.com>
2023-02-18 18:20:45 -0500

    Update Background.jsx

--- client/src/components/Background.jsx
+++ client/src/components/Background.jsx
@@ -1,4 +1,4 @@
-import animatedBackground from "../assets/Circuit Electric Colorful Moving Lines.mkv";
+import animatedBackground from "../assets/Circuit Electric Colorful Moving Lines.mp4";
 import StyledBackground from "./styles/StyledBackground";
 export default function Background() {
   return (
