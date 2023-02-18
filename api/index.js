057cb879209af9efe503c916c21720d60745fb3b
Juan José Rodriguez Valcke <zoranbow@gmail.com>
2023-02-18 01:14:57 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -19,10 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const PORT = PORT.process.env || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log('%s listening on port ', PORT); // eslint-disable-line no-console
   });
 });
