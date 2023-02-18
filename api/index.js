dee9aaa7b5bfae6a4d577731edf65ca2870c6d9f
Juan José Rodriguez Valcke <zoranbow@gmail.com>
2023-02-18 01:18:50 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require("dotenv").config();
-const PORT = PORT.process.env || 3001;
+const PORT = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
